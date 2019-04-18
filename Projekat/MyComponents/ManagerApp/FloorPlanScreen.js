d43b1054a2448d61a97447ad09f74d8d495a17ea
3-14 <pavle.stojanovic@elfak.rs>
2019-04-18 12:44:25 +0200

    dodavanje stolova

--- Projekat/MyComponents/ManagerApp/FloorPlanScreen.js
+++ Projekat/MyComponents/ManagerApp/FloorPlanScreen.js
@@ -2,17 +2,46 @@ import React from 'react';
 import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 import {Header,Left,Icon} from 'native-base';
 import styles from '../../styles'
+import Draggable from 'react-native-draggable';
 
 export default class FloorPlanScreen extends React.Component {
   
   constructor(){
     super();
+    this.state={
+        form:true,
+        firstPlan:false,//50
+        secondPlan:false,//100
+        thirdPlan:false,//150
+
+    }
   }
    render() {
+     var p=true;
+     if(p){
     return (
-    <View><Text>hii</Text></View>
+    <View>
+     
+      <Draggable reverse={false} renderSize={40}  offsetX={100} offsetY={0} renderColor='black' renderText='A' renderShape='req' pressDrag={()=>alert('touched!!')}/> 
+      <Draggable reverse={false} renderSize={80} renderColor='red' renderShape='image' imageSource={require( '../Images/sto.jpg')}  offsetX={0} offsetY={0} renderText='B'/>
+      <Draggable reverse={false} renderSize={46} renderColor='black' offsetX={-100} offsetY={200} renderText='A' pressDrag={()=>alert('touched!!')}/>
+      <Draggable reverse={false} renderSize={56} renderColor='black' offsetX={-100} offsetY={-200} renderText='A' pressDrag={()=>alert('touched!!')}/>
+      <Draggable reverse={false} renderSize={56} renderColor='black' offsetX={-100} offsetY={-200} renderText='A' pressDrag={()=>alert('touched!!')}/>
+    
+    </View>
     
     );
   }
+  else{
+    return (
+      <View>
+       
+        <Draggable reverse={false} renderSize={56} renderColor='black' offsetX={-100} offsetY={-200} renderText='A' pressDrag={()=>alert('touched!!')}/> 
+        
+      
+      </View>
+    );
+  }
+}
 }
 
