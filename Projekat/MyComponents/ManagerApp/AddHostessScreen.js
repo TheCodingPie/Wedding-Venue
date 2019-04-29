fb2377fa5f2051e6572a74863196fd66aec756d8
frufruJ <jovananikolicdd98@gmail.com>
2019-04-29 18:51:05 +0200

    Spojeno

--- Projekat/MyComponents/ManagerApp/AddHostessScreen.js
+++ Projekat/MyComponents/ManagerApp/AddHostessScreen.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import { StyleSheet, Text, View,TextInput,Button,TouchableOpacity } from 'react-native';
 import {Header,Left,Icon, DatePicker} from 'native-base';
@@ -6,24 +8,54 @@ import stylesAppFirstPage from '../stylesAppFirstPage';
 import FetchConstants from '../../Classes/fetchConstants';
 
 export default class AddHostessScreen extends React.Component {
-  
+
   constructor(){
-    super()
+    super();
     this.state={
-      name:"",
-      lastname:"",
-      code:""
-      }
- }
+      id:"",
+      dateAndTime:"",
+      name:""
+    }
+  }
+  handleTextChangeId=(newText)=>this.setState({id:newText});
+  handleTextChangeName=(newText)=>this.setState({name:newText});
+  handleTextChangeDateAndTime=(newText)=>this.setState({dateAndTime:newText});
+createHostess=()=>{
+  const formData=new FormData();
+  formData.append("createHostess",1);
+  formData.append("id",this.state.id);
+  formData.append("password",this.createPassword());
+  formData.append("name",this.state.name);
+  formData.append("dateAndTime",this.state.dateAndTime.toString());
+  formData.append("restaurantId",this.props.navigation.getParam('restaurantId',1111));
 
- handleChangeTextName=(newText)=>this.setState({name:newText});
- handleChangeTextLastname=(newText)=>this.setState({lastname:newText});
- handleChangeTextCode=(newText)=>this.setState({code:newText});
-
- btnUnesiPress=()=>{
-    alert("uneli ste hostesu");
-    //logika da se unese u bazu
-
+  const fetchData={
+    method:"post",
+    body:formData
+  };
+ 
+  fetch(FetchConstants.url+'/Manager.php',fetchData)
+  .then((response)=>response.json())
+  .then((response)=>{
+   if(response)
+   alert("Hostess added");
+   else
+   {
+    alert("Hostess not added please check your data");
+   }
+  })
+  .catch((error)=>{alert(error);});
+}
+createPassword(){
+  var length = 8;
+  let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let n=charset.length;
+  retVal = "";
+  for (var i = 0; i < length; i++) {
+  retVal += charset.charAt(parseInt(Math.random() * n));
+  }
+  return retVal;
+  
 }
 render() {
     return (
@@ -37,8 +69,6 @@ render() {
 </Header>
 </View>
 
-
-
 <View>
        <Text>Create Hostess</Text>
        <TextInput placeholder='Hostess id' style={stylesAppFirstPage.containerTextInput} placeholderTextColor='black' onChangeText={this.handleTextChangeId}></TextInput>
@@ -47,7 +77,6 @@ render() {
        <TouchableOpacity onPress={this.createHostess} style={stylesAppFirstPage.containerTO}><Text>Create Hostess</Text></TouchableOpacity>
 </View>  
 </View>
-
     );
   }
 }
