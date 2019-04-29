bb97df3dc48a86258fd0a432747f3744dd4b82cb
3-14 <pavle.stojanovic@elfak.rs>
2019-04-29 15:29:39 +0200

    Merge kalendar

--- Projekat/MyComponents/ManagerApp/FreeDaysScreen.js
+++ Projekat/MyComponents/ManagerApp/FreeDaysScreen.js
@@ -1,85 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View,TextInput,Button } from 'react-native';
 import {Header,Left,Icon} from 'native-base';
-import {Calendar} from 'react-native-calendars';
-
-
-let markedDates=[];
-let newDaysObject= {};
-
 export default class FreeDaysScreen extends React.Component {
   
   constructor(){
-    super();  
-       
-    markedDates.forEach((day) => {
-      newDaysObject = {
-        ...newDaysObject,
-        [day]: {
-          selected: true,
-          marked: true
-        }
-      };
-    });  
-    this.state={
-      markedD: newDaysObject,
-    };   
-   
-  }  
-
-   onPressDate(day)
-  {
-    markedDates.push(day);
-    /*markedDates.forEach((day) => {
-      newDaysObject = {
-        ...newDaysObject,
-        [day]: {
-          selected: true,
-          marked: true
-        }
-      };
-    });  */
-    newDaysObject = {
-      ...newDaysObject,
-      [day]: {
-        selected: true,
-        marked: true
-      }}; 
-
-    this.setState({ markedD: newDaysObject});
-
+    super();
   }
-  
-   render() {  
-
- 
-
+    
+   render() {
     return (
-      <View style={styles.container} >
+      <View style={styles.form} >
       <Header>
     <Left>
     <Icon name='menu' onPress={()=>this.props.navigation.openDrawer()} />
 
-   </Left>
-  </Header>
+  </Left>
+</Header>
 
 
-   <View style={styles.calendar}>
-   <Calendar
-      onDayPress={(day)=>{this.onPressDate(day.dateString)}}
-      markedDates={this.state.markedD}
-       theme={{  backgroundColor: '#ffffff'}}
-       />
-       
-   </View>
+<View style={styles.container}>
+       <Text>forma za slobodne dane</Text>
+</View>
 </View>   
     );
-    
   }
 }
-
 //za drawer navigator se razlikuju stilovi 
-/*
 const styles = StyleSheet.create({
     form: {
         flex: 1,
@@ -109,24 +55,4 @@ const styles = StyleSheet.create({
         backgroundColor: '#ecb3ff',
       },
      
-});*/
-const styles=StyleSheet.create({
-
-  container:{
-    flex: 1,
-  },
-  calendar:{
-    flex:1,
-    borderTopWidth: 1,  
-    borderBottomWidth:1,
-    height:'100%',
-    backgroundColor: 'green',
-    
-
-  },
-  heder:{
-    flex:0.5
-  }
-
-
-})
+});
